4e09acede7039f6b8b0bce4f1b7cb0a93b83d620
Yoonhwi <ush0105@naver.com>
2024-02-13 10:25:34 +0900

    Added function to check if scrolled to the bottom for infinite scroll

--- src/pages/group/[id]/feed/index.tsx
+++ src/pages/group/[id]/feed/index.tsx
@@ -1,21 +1,49 @@
-import { useFetch } from "@/apis";
+import { useFetch, useLoadMore } from "@/apis";
 import { useRouter } from "next/router";
 import { FeedWithPage } from "@/types";
 import { toUrl } from "@/utils";
 import { ApiRoutes } from "@/constants";
 import { GroupFeed } from "@/containers";
 import GroupDetail from "..";
+import { useEffect, useRef } from "react";
+import { Box, Button } from "@chakra-ui/react";
 
 const Feed = () => {
   const router = useRouter();
   const numberingQueryId = Number(router.query.id);
+  const containerRef = useRef<HTMLDivElement>(null);
+
   const { data: feeds, isSuccess } = useFetch<FeedWithPage>(
-    toUrl(ApiRoutes.GroupFeed, { id: numberingQueryId })
+    toUrl(ApiRoutes.GroupFeed, { id: numberingQueryId }),
+    {
+      cursor: 10,
+      limit: 10,
+    }
   );
+
+  useEffect(() => {
+    const container = containerRef.current;
+    if (!container) return;
+    function handleScroll() {
+      if (container) {
+        const { scrollTop, scrollHeight, clientHeight } = container;
+        if (scrollHeight - scrollTop === clientHeight) {
+          console.log("데이터요청");
+        }
+      }
+    }
+    container.addEventListener("scroll", handleScroll);
+    return () => {
+      container.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <GroupDetail>
       {isSuccess && feeds && (
-        <GroupFeed feeds={feeds.data} groupId={numberingQueryId} />
+        <Box ref={containerRef}>
+          <GroupFeed feeds={feeds.data} groupId={numberingQueryId} />
+        </Box>
       )}
     </GroupDetail>
   );
