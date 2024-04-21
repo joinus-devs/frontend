0287d18c030c30e67fbc037f05793271c8ca16eb
Yoonhwi <ush0105@naver.com>
2024-04-21 17:39:46 +0900

    fixed invalidateQueries error in CreateFeed

--- src/containers/group/CreateFeed/index.tsx
+++ src/containers/group/CreateFeed/index.tsx
@@ -1,10 +1,10 @@
 import { usePostFeed } from "@/apis/feed";
 import { ApiRoutes, PageRoutes } from "@/constants";
-import { Feed } from "@/types";
 import { QueryParser, toUrl } from "@/utils";
 import { useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/router";
 import PostFeed, { PostData } from "../PostFeed";
+import { useCallback } from "react";
 
 const CreateFeed = () => {
   const queryClient = useQueryClient();
@@ -12,16 +12,29 @@ const CreateFeed = () => {
   const clubId = QueryParser.toNumber(router.query.id);
   const { mutate } = usePostFeed(clubId);
 
+  const delay = (cb: Function) => {
+    const setDelay = setTimeout(() => {
+      cb();
+    }, 2000);
+    return () => clearTimeout(setDelay);
+  };
+
+  const cb = useCallback(() => {
+    delay(async () => {
+      await queryClient.invalidateQueries({
+        queryKey: [
+          toUrl(ApiRoutes.GroupFeed, {
+            id: clubId,
+          }),
+        ],
+      });
+    });
+  }, [clubId, queryClient]);
+
   const onSubmit = (values: PostData) => {
     mutate(values, {
       onSuccess: async () => {
-        await queryClient.invalidateQueries({
-          queryKey: [
-            toUrl(ApiRoutes.GroupFeed, {
-              id: clubId,
-            }),
-          ],
-        });
+        await cb();
         router.push(
           toUrl(PageRoutes.GroupFeed, {
             id: clubId,
